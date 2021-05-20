d6b30cf247c9822441f57b6cc35393b2570b40df
eman hamid <emanhharki@gmail.com>
2021-05-20 12:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+
+const arr = [1, 2, 3, -9];
+const mapToNegativize = arr.map(function(arr){
+  for (var i = 0; i < arr.length; i++) {
+      if(arr[i]<0){
+        arr[-i] = arr[i]
+      }
+      else if(arr[i]<0){
+        arr[i] = arr[-i]
+      }
+  }
+})
+console.log(mapToNegativize);
